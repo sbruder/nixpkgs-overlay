76b81d31bb1f703d346086310fe9231fec1d37b2
Simon Bruder <simon@sbruder.de>
2021-07-22 11:14:28 +0200

    make overlay work without flakes

---
+++ default.nix
@@ -0,0 +1,47 @@
+final: prev:
+let
+  inherit (prev) callPackage;
+
+  callPythonPackage = prev.python3Packages.callPackage;
+in
+{
+  python3 = prev.python3.override {
+    packageOverrides = final: prev:
+      {
+        deezer-py = callPythonPackage ./python-modules/deezer-py { };
+
+        enlighten = callPythonPackage ./python-modules/enlighten { };
+
+        prefixed = callPythonPackage ./python-modules/prefixed { };
+      };
+  };
+  python3Packages = prev.recurseIntoAttrs final.python3.pkgs;
+
+  cyanrip = callPackage ./cyanrip { };
+
+  deemix = callPythonPackage ./deemix { };
+
+  fSpy = callPackage ./fSpy { };
+
+  face_morpher = callPythonPackage ./face_morpher { };
+
+  mpvScripts = prev.mpvScripts // {
+    pitchcontrol = callPackage ./mpv-scripts/pitchcontrol { };
+  };
+
+  nsz = callPythonPackage ./nsz { };
+
+  oha = callPackage ./oha { };
+
+  snownews = callPackage ./snownews { };
+
+  textidote = callPackage ./textidote { };
+
+  unxwb = callPackage ./unxwb { };
+
+  vgmstream = callPackage ./vgmstream { };
+
+  VisiCut = callPackage ./VisiCut { };
+
+  x264-unstable = prev.x264.overrideAttrs (callPackage ./x264-unstable { });
+}
--- flake.nix
+++ flake.nix
@@ -10,52 +10,7 @@
   };
 
   outputs = { self, flake-utils, nixpkgs, nix-pre-commit-hooks }: {
-    overlay = final: prev:
-      let
-        inherit (prev) callPackage;
-        callPythonPackage = prev.python3Packages.callPackage;
-      in
-      {
-        python3 = prev.python3.override {
-          packageOverrides = final: prev:
-            {
-              deezer-py = callPythonPackage ./python-modules/deezer-py { };
-
-              enlighten = callPythonPackage ./python-modules/enlighten { };
-
-              prefixed = callPythonPackage ./python-modules/prefixed { };
-            };
-        };
-        python3Packages = prev.recurseIntoAttrs final.python3.pkgs;
-
-        cyanrip = callPackage ./cyanrip { };
-
-        deemix = callPythonPackage ./deemix { };
-
-        fSpy = callPackage ./fSpy { };
-
-        face_morpher = callPythonPackage ./face_morpher { };
-
-        mpvScripts = prev.mpvScripts // {
-          pitchcontrol = callPackage ./mpv-scripts/pitchcontrol { };
-        };
-
-        nsz = callPythonPackage ./nsz { };
-
-        oha = callPackage ./oha { };
-
-        snownews = callPackage ./snownews { };
-
-        textidote = callPackage ./textidote { };
-
-        unxwb = callPackage ./unxwb { };
-
-        vgmstream = callPackage ./vgmstream { };
-
-        VisiCut = callPackage ./VisiCut { };
-
-        x264-unstable = prev.x264.overrideAttrs (callPackage ./x264-unstable { });
-      };
+    overlay = import ./default.nix;
   } // flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
