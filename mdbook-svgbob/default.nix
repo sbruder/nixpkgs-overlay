a9bee40e00f2b8c0ba47941b5d872e19f4a72fb5
xav-ie <github@xav.ie>
2025-05-03 09:18:26 -0400

    mdbook-svgbob: update & add darwin support

--- mdbook-svgbob/default.nix
+++ mdbook-svgbob/default.nix
@@ -2,22 +2,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mdbook-svgbob";
-  version = "0.2.1";
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "boozook";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-5wfFT/7yc+55fcEi/KvmN1aR+hhUQi+0WdZukeD3xG4=";
+    hash = "sha256-5V8mTyIyKS/AgfGIM8oiVFvq/9cBlmxhUuBir71GElQ=";
   };
 
-  cargoHash = "sha256-jMSwqE4XAz0fG4sG3Cgtj9OhlqwL0N9aGgpRaEtC09Q=";
+  cargoHash = "sha256-0tWRX4Nzp/ExkAY5MVMsNPlRwxW8ZUm1V27mXA4UvyU=";
 
   meta = {
     description = "SvgBob mdbook preprocessor which swaps code-blocks with SVG";
     homepage = "https://github.com/boozook/mdbook-svgbob";
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ sbruder ];
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.darwin ++ lib.platforms.linux;
   };
 }
