2bf06925dd166544a8b617500f87203ef9158a74
Simon Bruder <simon@sbruder.de>
2026-04-08 00:32:53 +0200

    feishin-web: 1.9.0 -> 1.11.0

--- feishin-web/default.nix
+++ feishin-web/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "feishin";
-  version = "1.9.0";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "jeffvli";
     repo = finalAttrs.pname;
     rev = "v${finalAttrs.version}";
-    hash = "sha256-LcSe7aEtTDs4JIk50zI0IFgN4ZCSJ6NClfk02uO2Sg8=";
+    hash = "sha256-TSjgjNHhPSZ4k7zZTH5e3FCkl6d7B/2w2WCt0S5OW0g=";
   };
 
   nativeBuildInputs = [
@@ -19,7 +19,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = pnpm_10.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = "sha256-cGIdcMcBIoSbtaN4ZcS/hM0fVsW08fqac4LIlpj/nb4=";
+    hash = "sha256-9Ecbvr8yMX0fs7wf5UjVRq6kx/PjAYK1hwKHKf9kgys=";
   };
 
   buildPhase = ''
