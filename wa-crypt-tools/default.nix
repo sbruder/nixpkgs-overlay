2c8302f6b39f86b6c64843a354602b482a187bf6
Simon Bruder <simon@sbruder.de>
2026-04-10 14:25:14 +0200

    wa-crypt-tools: 0.0.8 -> 0.1.0

--- wa-crypt-tools/default.nix
+++ wa-crypt-tools/default.nix
@@ -1,63 +1,43 @@
-#{ lib
-#, buildPythonPackage
-#, fetchPypi
-#, setuptools
-#, javaobj-py3
-#, protobuf
-#}:
-#
-#buildPythonPackage rec {
-#  pname = "wa-crypt-tools";
-#  version = "0.0.8";
-#
-#  src = fetchPypi {
-#    inherit pname version;
-#    sha256 = "sha256-H5uSVKtmcR+xZ+CWgpfUyF+YdO/s4VB7YV6r0ZX39/8=";
-#  };
-#
-#  format = "pyproject";
-#
-#  nativeBuildInputs = [
-#    setuptools
-#  ];
-#
-#  propagatedBuildInputs = [
-#    javaobj-py3
-#    prtobuf
-#  ];
-#
-#  meta = with lib; {
-#    description = "Decryptor for WhatsApp’s databases";
-#    homepage = "https://github.com/ElDavoo/wa-crypt-tools";
-#    license = licenses.gpl3Only;
-#    maintainers = with maintainers; [ sbruder ];
-#    platforms = platforms.all;
-#  };
-#}
-{ poetry2nix, pkgs, lib }:
-let
-  pythonPackages = (poetry2nix.mkPoetryPackages {
-    projectDir = ./.;
-    overrides = [
-      poetry2nix.defaultPoetryOverrides
-      #(import ./poetry-git-overlay.nix { inherit pkgs; })
-      (final: prev: {
-        wa-crypt-tools = prev.wa-crypt-tools.overridePythonAttrs (old: {
-          propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ prev.setuptools ];
-          #postInstall = ''
-          #  install -D ${old.pname}.py $out/bin/${old.pname}
-          #'';
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, setuptools-scm
+, javaobj-py3
+, protobuf
+, pycryptodomex
+, pythonRelaxDepsHook
+}:
 
-          meta = old.meta // (with lib; {
-            description = "Decryptor for WhatsApp’s databases";
-            homepage = "https://github.com/ElDavoo/wa-crypt-tools";
-            license = licenses.gpl3Only;
-            maintainers = with maintainers; [ sbruder ];
-            platforms = platforms.all;
-          });
-        });
-      })
-    ];
-  }).python.pkgs;
-in
-pythonPackages.wa-crypt-tools
+buildPythonPackage rec {
+  pname = "wa_crypt_tools";
+  version = "0.1.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-E/k2lKxlBxUDnfbeELHBi3sML3T+BQJ3zPHh4hGviJ4=";
+  };
+
+  format = "pyproject";
+  build-system = [ setuptools setuptools-scm ];
+
+  propagatedBuildInputs = [
+    javaobj-py3
+    protobuf
+    pycryptodomex
+  ];
+  nativeBuildInputs = [
+    pythonRelaxDepsHook
+  ];
+  pythonRelaxDeps = [
+    "protobuf"
+  ];
+
+  meta = with lib; {
+    description = "Decryptor for WhatsApp’s databases";
+    homepage = "https://github.com/ElDavoo/wa-crypt-tools";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ sbruder ];
+    platforms = platforms.all;
+  };
+}
